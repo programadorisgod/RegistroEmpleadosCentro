3a86b60248874e7091bbc52f2cc9d9bfbf45a47d
programadorisgod <catapiascastro@gmail.com>
2022-11-23 17:26:34 -0500

    Agregar archivos de proyecto.

---
+++ Entidades/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Persona
+    {
+        public string Cedula { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Telefono { get; set; }
+        public Persona()
+        {
+
+        }
+        public Persona(string Cedula, string Nombre, string Telefono)
+        {
+            this.Cedula = Cedula;
+            this.Nombre = Nombre;
+            this.Telefono = Telefono;
+
+        }
+    }
+}
